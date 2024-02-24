f64c36722bcfd3993432c051a5a227cd1ed3b450
Babygungle <apredikanit@gmail.com>
2024-02-24 23:12:45 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="Header w-96 h-96 relative">
+    <div className="Header w-96 h-96 left-[-64.27px] top-0 absolute">
+      <div className="Bg w-96 h-96 left-[64.27px] top-0 absolute bg-blue-50" />
+      <div className="Text w-96 h-96 left-[314.55px] top-[303px] absolute">
+        <div className="Group1566 w-48 h-16 left-0 top-[424px] absolute">
+          <div className="Rectangle695 w-48 h-16 left-0 top-0 absolute bg-indigo-800 rounded-2xl" />
+          <div className="GetAQuotes left-[44px] top-[25px] absolute text-center text-white text-base font-bold font-['Circular Std']">Get a Quotes</div>
+        </div>
+        <div className="WeHelpYouToBuildYourBusiness w-96 left-0 top-0 absolute text-blue-950 text-7xl font-black font-['Circular Std'] leading-10">We help you<br/>to build your Business</div>
+        <div className="CreateCustomLandin w-96 left-0 top-[294px] absolute text-slate-500 text-xl font-['Circular Std'] leading-10">Enthusiastically extend extensive customer service before best-of-breed convergence completely.</div>
+      </div>
+      <div className="Group1570 w-96 h-96 left-[1048.87px] top-[152.69px] absolute">
+        <div className="Group1568 w-96 h-96 left-0 top-[97.16px] absolute">
+          <div className="Ellipse984 w-96 h-96 left-[78.73px] top-[78.73px] absolute rounded-full border border-stone-300" />
+          <div className="Ellipse985 w-96 h-96 left-0 top-0 absolute rounded-full border border-stone-300" />
+        </div>
+        <div className="Image w-96 h-96 left-[43.61px] top-0 absolute">
+          <div className="Placeholder w-96 h-96 left-0 top-0 absolute bg-stone-300" />
+        </div>
+      </div>
+      <div className="Group1580 w-56 h-28 left-[1005.23px] top-[371.66px] absolute">
+        <div className="Rectangle717 w-56 h-28 left-0 top-0 absolute bg-white rounded-lg shadow" />
+        <div className="Group1579 w-44 h-16 left-[24px] top-[24px] absolute">
+          <div className="KCustomerReview left-0 top-[41px] absolute text-slate-500 text-base font-medium font-['Circular Std']">20k+ Customer Review</div>
+          <div className="Group1578 w-36 h-9 left-0 top-0 absolute">
+            <div className="Ellipse978 w-9 h-9 left-0 top-0 absolute bg-stone-300 rounded-full border border-white" />
+            <div className="Group1577 w-24 h-4 left-[46px] top-[9px] absolute">
+              <div className="AntDesignStarFilled w-4 h-4 left-0 top-0 absolute" />
+              <div className="AntDesignStarFilled w-4 h-4 left-[20px] top-0 absolute" />
+              <div className="AntDesignStarFilled w-4 h-4 left-[40px] top-0 absolute" />
+              <div className="AntDesignStarFilled w-4 h-4 left-[60px] top-0 absolute" />
+              <div className="AntDesignStarFilled w-4 h-4 left-[80px] top-0 absolute" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Icon w-16 h-16 left-[1033.76px] top-[691.50px] absolute">
+        <div className="Rectangle700 w-16 h-16 left-0 top-0 absolute bg-cyan-500 rounded-3xl shadow" />
+        <div className="BxBxsCheckShield w-8 h-8 left-[20px] top-[20px] absolute" />
+      </div>
+      <div className="Group5828 w-52 h-40 left-[1563.29px] top-[735.21px] absolute">
+        <div className="Rectangle699 w-52 h-40 left-0 top-0 absolute bg-cyan-500 rounded-xl shadow" />
+        <div className="Group5827 w-36 h-28 left-[23.55px] top-[26.40px] absolute">
+          <div className="MonthlyVisitor w-32 h-5 left-0 top-0 absolute text-white text-base font-medium font-['Circular Std']">Monthly Visitor</div>
+          <div className=" w-3 h-3.5 left-0 top-[41.44px] absolute text-slate-200 text-xs font-['Circular Std']">30</div>
+          <div className=" w-3 h-3.5 left-0 top-[68.24px] absolute text-slate-200 text-xs font-['Circular Std']">20</div>
+          <div className=" w-3 h-3.5 left-0 top-[95.05px] absolute text-slate-200 text-xs font-['Circular Std']">10</div>
+          <div className="Rectangle736 w-3 h-11 left-[33.96px] top-[59.67px] absolute bg-white bg-opacity-50 rounded-tl-3xl rounded-tr-3xl" />
+          <div className="Rectangle737 w-3 h-14 left-[67.65px] top-[48.30px] absolute bg-white rounded-tl-3xl rounded-tr-3xl" />
+          <div className="Rectangle739 w-3 h-16 left-[135.03px] top-[41.44px] absolute bg-white rounded-tl-3xl rounded-tr-3xl" />
+          <div className="Rectangle738 w-3 h-11 left-[101.34px] top-[59.67px] absolute bg-white bg-opacity-50 rounded-tl-3xl rounded-tr-3xl" />
+        </div>
+      </div>
+      <div className="Group5836 w-36 h-24 left-[1640.94px] top-[455.23px] absolute">
+        <div className="Rectangle717 w-36 h-24 left-0 top-0 absolute bg-white rounded-xl shadow" />
+        <div className="Group5822 w-28 h-14 left-[20.34px] top-[22.89px] absolute">
+          <div className="Group5784 w-24 h-14 left-0 top-0 absolute">
+            <div className="ProjectDone w-24 h-6 left-0 top-0 absolute text-slate-500 text-lg font-['Circular Std']">Project Done</div>
+            <div className="258 w-14 h-7 left-0 top-[26.27px] absolute text-blue-950 text-2xl font-bold font-['Circular Std']">3,258</div>
+          </div>
+        </div>
+      </div>
+      <div className="Frame1 w-96 h-36 left-[64.55px] top-0 absolute">
+        <div className="Group1563 w-96 h-16 left-[250px] top-[38px] absolute">
+          <div className="Menu left-[218.71px] top-[20.50px] absolute opacity-80 text-blue-950 text-lg font-['Circular Std']">About          Service          Blog          Contact          Pricing</div>
+          <div className="Agency left-0 top-0 absolute text-indigo-800 text-4xl font-bold font-['Baloo Bhai 2']">Agency</div>
+        </div>
+        <div className="Group1565 w-32 h-16 left-[1536px] top-[38px] absolute">
+          <div className="Rectangle695 w-32 h-16 left-0 top-0 absolute bg-blue-50 rounded-2xl border border-blue-950" />
+          <div className="LogIn left-[44px] top-[22px] absolute text-center text-blue-950 text-base font-bold font-['Circular Std']">Log In</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default App
