d438e573080063066ea4a905130502df3aaedd0b
Babygungle <144519233+Babygungle@users.noreply.github.com>
2024-03-31 01:58:24 +0700

    feat: create testimonial section

---
+++ src/components/testimonial.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+export default function Testimonial() {
+  return (
+    <div className="py-8">
+      <div className="rounded-full w-[558.54px] h-[558.54px] max-md:w-[500px] max-md:h-[500px] max-sm:w-[450px] max-sm:h-[450px] ml-[97px] max-lg:ml-[30px] mt-10 max-md:mt-[-1510px] max-md:ml-[30px] max-sm:ml-[-20px] bg-gradient-to-r max-md:bg-gradient-to-b p-[1px] from-[#152fd844] via-[#f8fbff00] to-[#ffffff58]">
+        <div className="flex flex-col justify-center h-full rounded-full bg-white"></div>
+        <div className="rounded-full w-[502.68px] h-[502.68px] max-md:w-[500px] max-md:h-[500px] max-sm:w-[450px] max-sm:h-[450px] ml-[60px] max-lg:ml-[60px] mt-[-520px] max-md:mt-[-450px] max-sm:mt-[-400px] max-md:ml-[4px] bg-gradient-to-r max-md:bg-gradient-to-b p-[1px] from-[#1566d814] via-[#f8fbff0e] to-[#ffffff58]">
+          <div className="flex flex-col justify-center h-full rounded-full bg-white"></div>
+        </div>
+
+        <div className="rounded-full w-[448.98px] h-[448.98px] max-md:w-[500px] max-md:h-[500px] max-sm:w-[450px] max-sm:h-[450px] ml-[135px] max-lg:ml-[130px] mt-[-480px] max-md:mt-[-460px] max-sm:mt-[-400px] max-md:ml-[5px] bg-gradient-to-r max-md:bg-gradient-to-b p-[1px] from-[#1566d82b] via-[#f8fbff0e] to-[#ffffff58]">
+          <div className="flex flex-col justify-center h-full rounded-full bg-white"></div>
+        </div>
+        <div className="rounded-full w-[558.54px] h-[558.54px] ml-[500px] max-md:w-[500px] max-md:h-[500px] max-sm:w-[450px] max-sm:h-[450px] max-lg:ml-[300px] max-md:mt-[100px] max-sm:mt-[160px] max-md:ml-[-5px] mt-[-497px] bg-gradient-to-r max-md:bg-gradient-to-b p-[1px] from-[#ffffff58] via-[#f8fbff0e] to-[#152fd834]">
+          <div className="flex flex-col justify-center h-full rounded-full bg-white"></div>
+        </div>
+        <div className="rounded-full w-[502.68px] h-[502.68px] ml-[480px] max-md:w-[500px] max-md:h-[500px] max-sm:w-[450px] max-sm:h-[450px] max-lg:ml-[280px] mt-[-530px] max-md:mt-[-550px] max-sm:mt-[-510px] max-md:ml-[-5px] bg-gradient-to-r max-md:bg-gradient-to-b p-[1px] from-[#ffffff58] via-[#f8fbff0e] to-[#1566d82b] ">
+          <div className="flex flex-col justify-center h-full rounded-full bg-white"></div>
+        </div>
+        <div className="rounded-full w-[448.98px] h-[448.98px] max-md:w-[500px] max-md:h-[500px] ml-[450px] max-sm:w-[450px] max-sm:h-[450px] max-lg:ml-[260px] mt-[-480px] max-md:mt-[-550px] max-sm:mt-[-504px] max-md:ml-[-5px] bg-gradient-to-r max-md:bg-gradient-to-b p-[1px] from-[#ffffff58] via-[#f8fbff0e] to-[#1566d82b] ">
+          <div className="flex flex-col justify-center h-full rounded-full bg-white"></div>
+        </div>
+        <div
+          class="circle-1 rounded-full w-[106px] h-[106px]  bg-[#C4C4C4]
+      ml-[10px] mt-[-460px] max-lg:mt-[-440px] max-lg:w-[90px] max-lg:h-[90px] max-md:mt-[-1080px] max-sm:mt-[-1040px] max-sm:ml-[280px] max-sm:absolute
+      max-md:w-[50px] max-md:h-[50px] max-md:ml-[340px] max-md:text-sm"
+        ></div>
+      </div>
+      <div
+        class="circle-2 rounded-full w-[102px] h-[102px] bg-[#C4C4C4]
+      ml-[180px] mt-[-350px] max-lg:ml-[140px] max-lg:mt-[-330px] max-lg:w-[90px] max-lg:h-[90px] max-md:mt-[-440px] max-sm:mt-[-390px] max-sm:ml-[80px] max-sm:absolute 
+      max-md:w-[40px] max-md:h-[40px] max-md:ml-[180px] max-md:text-sm"
+      ></div>
+      <div
+        class="circle-3 rounded-full w-[80px] h-[80px] md:w-[60px] md:h-[60px] bg-[#C4C4C4]
+      ml-[170px] mt-[80px] max-lg:ml-[100px] max-lg:mt-[90px] max-lg:w-[80px] max-lg:h-[80px] max-md:mt-[-30px] max-sm:mt-[-370px] max-sm:ml-[200px] max-sm:absolute
+      max-md:w-[50px] max-md:h-[50px] max-md:ml-[325px] max-md:text-sm"
+      ></div>
+
+      <div
+        class="circle-4 rounded-full w-[90px] h-[90px] bg-[#C4C4C4]
+      ml-[980px] mt-[-400px] max-lg:ml-[740px] max-lg:mt-[-390px] max-lg:w-[90px] max-lg:h-[90px] max-md:mt-[930px] max-sm:mt-[540px] max-sm:ml-[80px] max-sm:absolute 
+      max-md:w-[50px] max-md:h-[50px] max-md:ml-[200px] max-md:text-sm"
+      ></div>
+      <div
+        class="circle-5 rounded-full w-[100px] h-[100px] bg-[#C4C4C4]
+      ml-[1090px] mt-[80px] max-lg:ml-[800px] max-lg:mt-[100px] max-lg:w-[100px] max-lg:h-[100px] max-md:mt-[-10px] max-sm:mt-[610px] max-sm:ml-[260px] max-sm:absolute
+      max-md:w-[40px] max-md:h-[40px] max-md:ml-[380px] max-md:text-sm"
+      ></div>
+      <div
+        class="circle-6 rounded-full w-[105px] h-[105px] bg-[#C4C4C4]
+      ml-[986px] mt-[50px] max-lg:ml-[680px] max-lg:mt-[5px] max-lg:w-[90px] max-lg:h-[90px] max-md:mt-[24px] max-sm:mt-[680px] max-sm:ml-[160px] max-sm:absolute
+      max-md:w-[50px] max-md:h-[50px] max-md:ml-[270px] max-md:text-sm"
+      ></div>
+      <div class="text-box text-center px-[360px] my-[-260px] max-lg:my-[-340px] max-lg:mx-[-40px] max-md:mt-[-1000px] max-sm:mt-[-240px] max-md:px-[250px] max-sm:px-[123px] leading-loose">
+        <p class="text-2xl max-md:text-3xl text-[#183B56] font-bold leading-9 max-md:leading-[80px] max-lg:mx-[-90px] max-lg:my-[-30px]">
+          “I’ve completed support from my Agency about 1 years ago. Their make
+          the prototyping process so easy and faster. I feel this is the best
+          apps for the prototyping use”
+        </p>
+        <br></br>
+        <p class="text-sm max-md:text-xl text-[#0BC0EB] font-bold leading-10">
+          William Smith
+        </p>
+        <p class="text-sm max-md:text-xl text-[#5A7184]">CEO f ZaVad Company</p>
+      </div>
+    </div>
+  );
+}
